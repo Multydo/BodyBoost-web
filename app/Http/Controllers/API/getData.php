ae2d188a3523f40530b76f60633bab738db3a6fa
Nour Achraf Badreddine <nourbdredine@gmail.com>
2024-09-13 19:13:09 +0300

    nour

--- app/Http/Controllers/API/getData.php
+++ app/Http/Controllers/API/getData.php
@@ -8,7 +8,8 @@ use App\Models\Exercise;
 
 class getData extends Controller
 {
-    public function getBodyPart(Request $request){
+    public function getBodyPart(Request $request){ 
+        
         $bodyPart = $request["body_part"];
         if($request["lastId"]==-1){
            $firstId = Exercise::where('body_part', $bodyPart)
@@ -54,6 +55,127 @@ class getData extends Controller
         }else{
             return response()->json([
                 "message"=>"invalid body_part: $bodyPart or id: $reqId",
+                "state"=>false,
+                "data"=>""
+            ],400);
+        }
+       
+    }
+}
+
+
+
+
+
+
+
+
+class getData extends Controller
+{
+    public function getfunction(Request $request){
+        
+        $function = $request["function"];
+        if($request["lastId"]==-1){
+           $firstId = Exercise::where('function', $function)
+                        ->orderBy('id', 'asc')
+                        ->select('id')
+                        ->first();
+
+            $reqId = $firstId["id"];
+        }else{
+            $reqId = $request["lastId"]+1 ;
+        }
+
+
+        
+        $exerciseData = Exercise::where([
+                                ['function', '=', $fuction],
+                                ['id', '>=', $reqId]
+                            ])->take(10)
+                            ->select('id', 'name', 'body_part', 'equipment', 'target', 'secondary_muscles', 'instructions')
+                            ->get()
+                            ->toArray();
+        //dd($exerciseData);
+        if($exerciseData){
+            $moreId = $reqId +11 ;
+            $moreExData = Exercise::where([
+                                ['function', '=', $function],
+                                ['id', '>=', $moreId]
+                            ])->take(10)
+                            ->select('id', 'name', 'body_part', 'equipment', 'target', 'secondary_muscles', 'instructions')
+                            ->get()
+                            ->toArray();
+            $state = true;
+            if(!$moreExData){
+                $state = false;
+            }   
+            //dd($exerciseData);
+            return response()->json([
+                "message" => "exercise data found",
+                "state" => $state,
+                "data" => $exerciseData
+            ],200);
+
+        }else{
+            return response()->json([
+                "message"=>"invalid function: $function or id: $reqId",
+                "state"=>false,
+                "data"=>""
+            ],400);
+        }
+       
+    }
+}
+
+
+class getData extends Controller
+{
+    public function getmachine(Request $request){
+        $function = $request["machine"];
+        if($request["lastId"]==-1){
+           $firstId = Exercise::where('machine', $machine)
+                        ->orderBy('id', 'asc')
+                        ->select('id')
+                        ->first();
+
+            $reqId = $firstId["id"];
+        }else{
+            $reqId = $request["lastId"]+1 ;
+        }
+
+
+        
+        $exerciseData = Exercise::where([
+                                ['machine', '=', $fuction],
+                                ['id', '>=', $reqId]
+                            ])->take(10)
+                            ->select('id', 'name', 'body_part', 'equipment', 'target', 'secondary_muscles', 'instructions')
+                            ->get()
+                            ->toArray();
+        //dd($exerciseData);
+        if($exerciseData){
+            $moreId = $reqId +11 ;
+            $moreExData = Exercise::where([
+                                ['machine', '=', $machine],
+                                ['id', '>=', $moreId]
+                            ])->take(10)
+                            ->select('id', 'name', 'body_part', 'equipment', 'target', 'secondary_muscles', 'instructions')
+                            ->get()
+                            ->toArray();
+            $state = true;
+            if(!$moreExData){
+                $state = false;
+            }   
+            //dd($exerciseData);
+            return response()->json([
+                "message" => "exercise data found",
+                "state" => $state,
+                "data" => $exerciseData
+            ],200);
+
+        }else{
+            return response()->json([
+                "message"=>"invalid machine: $machine or id: $reqId",
                 "state"=>false,
                 "data"=>""
             ],400);
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\getData;
+route::post('functions',[getData::class,'getfunction']);
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+route::post('machines',[getData::class,'getmachine']);
+
+route::post('bodyParts',[getData::class,'getBodyPart']);
